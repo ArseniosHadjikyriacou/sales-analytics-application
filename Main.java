a00a0d13360d653adc120f58488728e246686112
Arsenios Hadjikyriacou <a.hadjikyriacou@deltasoftsolutions.net>
2025-08-02 21:25:01 +0300

    Initial IO setup

---
+++ Main.java
@@ -0,0 +1,84 @@
+import java.io.File;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.FileNotFoundException;
+
+
+public class Main {
+
+
+  public static void main(String[] args) {
+    ArrayList<String> sales = new ArrayList<String>();
+
+    // Get the file paths from command line arguments
+    if (args.length == 0) {
+      System.out.println("Usage: java Main <path/to/file1> <path/to/file2> ...");
+      return;
+    }
+    String[] filePaths = args;
+    for (int i = 0; i < filePaths.length; i++) {
+      // file.separator ensures compatibility across different OS
+      filePaths[i] = filePaths[i].replace("/", File.separator).replace("\\", File.separator);
+    }
+
+    for (String filePath : filePaths) {
+      try {
+        File dataFile = new File(filePath);
+        Scanner dataReader = new Scanner(dataFile);
+        while (dataReader.hasNextLine()) {
+          String data = dataReader.nextLine();
+          sales.add(data);
+        }
+        dataReader.close();
+      } catch (FileNotFoundException e) {
+        System.out.println("Please check that the specified filepath (" + filePath + ") is correct and that the file exists");
+        e.printStackTrace();
+        return;
+      }
+    }
+    System.out.println("Length of sales data: " + sales.size());
+    System.out.println();
+      
+
+    Scanner scanner = new Scanner(System.in);
+    String dateFormat = "";
+
+    while (true) {
+      System.out.println("Enter a valid date format.");
+      System.out.println("Supported formats: dd/MM/yyyy, MM/dd/yyyy, yyyy/MM/dd");
+      dateFormat = scanner.nextLine();
+
+      if (dateFormat.equals("dd/MM/yyyy") || dateFormat.equals("MM/dd/yyyy") || dateFormat.equals("yyyy/MM/dd")) {
+        System.out.println("You entered date format: " + dateFormat);
+        System.out.println();
+        break;
+      } else {
+        System.out.println("Invalid date format. Please try again.");
+        System.out.println();
+      }
+    }
+
+    String amountFormat = "";
+
+    while (true) {
+      System.out.println("Enter a valid amount format.");
+      System.out.println("Supported formats: integer, double, float");
+      amountFormat = scanner.nextLine();
+
+      if (amountFormat.equals("integer") || amountFormat.equals("double") || amountFormat.equals("float")) {
+        System.out.println("You entered amount format: " + amountFormat);
+        System.out.println();
+        break;
+      } else {
+        System.out.println("Invalid amount format. Please try again.");
+        System.out.println();
+      }
+    }
+    System.out.println("You have successfully set up the application with date format: " + dateFormat + " and amount format: " + amountFormat);
+    System.out.println();
+    scanner.close();
+
+  }
+
+
+}
